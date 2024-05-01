aca641f78cebf1a5b8bc88dc6d9787c6f7da720f
Long Tan Le <52901258+longtanle@users.noreply.github.com>
2024-05-01 22:34:07 +1000

    Add files via upload

--- w10/src/client.py
+++ w10/src/client.py
@@ -4,9 +4,24 @@ import socket
 from blockchain import make_signature, make_transaction
 from network import recv_prefixed, send_prefixed
 
-# TODO generate a private key and create a transaction using make_signature and make_transaction
-# using the derived public key as the sender
+print("Generating Transaction....\n")
 
-# TODO create a socket and connect to the blockchain node
+private_key = ed25519.Ed25519PrivateKey.generate()
+sender = private_key.public_key().public_bytes_raw().hex()
+message = 'hello'
+signature = make_signature(private_key, message)
+transaction = make_transaction(sender, message, signature)
 
-# TODO send the transaction, receive the response and print it
+print(transaction)
+
+print("\nSending Transaction....\n")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 8000))
+
+send_prefixed(s, transaction.encode())
+try:
+	data = recv_prefixed(s).decode()
+	print(data)
+except Exception as e:
+	print(e)
